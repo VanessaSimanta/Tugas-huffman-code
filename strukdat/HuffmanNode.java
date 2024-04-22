8e03705d2c47b032370cf0b4f776c8ed5e1c654c
VanessaSimanta <vanessasimanta@gmail.com>
2024-04-22 13:26:53 +0700

    2

---
+++ strukdat/HuffmanNode.java
@@ -0,0 +1,29 @@
+package strukdat;
+
+public class HuffmanNode extends Gnode<Integer, Character> {
+    TheArrayList<String> codeList;
+    public HuffmanNode(int freq, char letter, HuffmanNode node1, HuffmanNode node2) {
+        super(freq, letter);
+        llink = node1;
+        rlink = node2;
+    }
+
+    public TheArrayList<String> getHuffmanCodes(HuffmanNode root, int n) {
+        String s = " ";
+        codeList = new TheArrayList<String>(n);
+        printCode(root, s);
+        return;
+    }
+
+    //print code (deretan bit)​
+    public void printCode(HuffmanNode node, String s) {
+        if (...) {//jika node adalah child node​
+            codeList.add(node.data + " " + s);
+            return;
+        }
+        printCode((HuffmanNode)node.llink, s + "1");
+        printCode((HuffmanNode)node.rlink, s +"1"); //traverse ke subtree kanan​
+    }
+}
+
+
