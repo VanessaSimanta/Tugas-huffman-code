638b6722111b4c2cec8eabfa24c9c82e532b9e8b
VanessaSimanta <vanessasimanta@gmail.com>
2024-05-05 19:52:27 +0700

    final

--- strukdat/HuffmanCoding.java
+++ strukdat/HuffmanCoding.java
@@ -4,14 +4,14 @@ public class HuffmanCoding {
     public static void main(String[] args) {
         char[] charArray = { 'E', 'T', 'N', 'I', 'S'};
         int[] charfreq = { 29, 10, 9, 5, 4};
-        //char[] charArray = {'A', 'F', 'I', 'K', 'M', 'N', 'O', 'R', 'T'};
-        //int[] charfreq ={5, 9, 3, 25, 39, 20, 11, 15, 14 };
+        // char[] charArray = {'A', 'F', 'I', 'K', 'M', 'N', 'O', 'R', 'T'};
+        // int[] charfreq ={5, 9, 3, 25, 39, 20, 11, 15, 14 };
         
-        // Create a priority queue with a min-heap
-        Heap<Integer, HuffmanNode> pq = new Heap<Integer, HuffmanNode>(charArray.length, true);
+        // buat priority queue dengan heap min
+        Heap<Integer, HuffmanNode> pq = new Heap<Integer, HuffmanNode>(charfreq.length, true);
         
-        // Input each character and its frequency into the priority queue
-        for(int i = 0; i < charArray.length; i++){
+        // inputkan setiap huruf dan frekuensinya ke pq
+        for(int i = 0; i < charfreq.length; i++){
             pq.insert(charfreq[i], new HuffmanNode(charfreq[i],charArray[i], null, null));
         }
         
@@ -23,28 +23,31 @@ public class HuffmanCoding {
 
        while(pq.size() > 1) {
         sum = pq.getKey(pq.first());
-        x = pq.getData(pq.removeFirst()); // Remove the first minimum node
-        y = pq.getData(pq.removeFirst()); // Remove the second minimum node
-    
+        x = pq.getData(pq.first()); 
+        // y = pq.getData(pq.removeFirst()); 
+        pq.removeFirst();
+        sum += pq.getKey(pq.first());
+        y = pq.getData(pq.first());
+        pq.removeFirst();
+
         root = new HuffmanNode(sum, '-', x, y);
         pq.insert(sum, root);
        }
 
+    root = pq.getData(pq.first());
     TheArrayList<String> huffmanCodes = root.getHuffmanCodes(root, charArray.length);
        System.out.println("---------------------");
        System.out.println(" Huruf | Huffman code ");
        System.out.println("---------------------");
        for(int i = 0; i < huffmanCodes.size(); i++) {
-        // split each string in ArrayList to get the character and Huffman code
+        // split setiap string di Arraylist untuk mendapatkan huruf dan huffman codenya
         String parts = huffmanCodes.get(i);
         String[] kata = parts.split(":");
 
-        // display the character and Huffman code
+        //tampilkan huruf dan huffman codenya 
         System.out.println(kata[0] + " | " + kata[1]);
-    }  
+        }  
        System.out.println("---------------------");
     }
 }
-
-
 
